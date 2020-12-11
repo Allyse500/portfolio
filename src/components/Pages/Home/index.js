8adedd67161ac36937e0680f4b1b9ae64eb22e02
Allyse <ayubired@gmail.com>
2020-12-10 23:56:37 -0600

    enabled self photo

--- src/components/Pages/Home/index.js
+++ src/components/Pages/Home/index.js
@@ -4,6 +4,10 @@ import React from 'react';
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AboutContent from "./About/AboutContent";
+// import MyPhoto from "./Photo/myPhoto.jpg";
+// import Resizer from 'react-image-file-resizer';
+
+// Resizer.imageFileResizer(MyPhoto, 300, 300, 'PNG')
 
 const Home = () => {
 return (   
@@ -11,6 +15,7 @@ return (
         <div className="left">
             <h1>Allyse D. Johnson</h1>
         </div>
+        {/* <img src= {MyPhoto} alt="MyPhoto"/> */}
         <AboutContent/>
     </div>
 );
