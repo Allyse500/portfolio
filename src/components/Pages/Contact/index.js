b3a1facb90d6a970d58ce5ecd31848fee3cd9db6
Allyse <ayubired@gmail.com>
2020-12-12 16:09:25 -0600

    added content to contact page

--- src/components/Pages/Contact/index.js
+++ src/components/Pages/Contact/index.js
@@ -1,8 +1,31 @@
-// import React from 'react';
+import React from 'react';
 
 import "../../../index.css"
 
 const Contact = () => {
-    return "Contact";}
+    const style ={
+        color: "rgb(63, 23, 63)",
+        backgroundColor: "rgb(12, 207, 221, 0.5)",
+        padding: "10px",
+        fontFamily: "Arial",
+        textAlign: "center",
+        position: "absolute",
+        top: "23%",
+        left: "40%",
+        width: "50%",
+        fontSize: "200%",
+        border: "2px double gold",
+        borderWidth: "5px",
+        borderRadius: "8px"
+      }
+    return (
+        <div style = {style}>
+            <h2 style ={{fontSize:"235%"}}>Contact Information</h2>
+            <p>Phone: 737-224-1626</p>
+            <p>email: ayubired@gmail.com</p>
+            <p>Find me on <a style={{color: "green"}} href= "https://www.linkedin.com/in/allyse-johnson-3b7a84116/">LinkedIn</a></p>
+        </div>
+        )
+        }
     
     export default Contact;
