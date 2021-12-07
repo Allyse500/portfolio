67b2d2b8c115d66dd6cf8e1db2f41ce3d205007a
Allyse <ayubired@gmail.com>
2021-12-07 16:25:57 -0600

    include new page for portfolio, update autobiography, insert open to new tab

--- src/components/Pages/Home/About/AboutContent.js
+++ src/components/Pages/Home/About/AboutContent.js
@@ -22,12 +22,12 @@ const AboutContent = () => {
         <h1 style ={{fontSize: "235%"}}>Autobiography</h1>     
         <p >My name is Allyse D. Johnson. I graduated from The University of 
         Texas at San Antonio in Dec. 2016 with a Bachelor's of Science in
-        Chemistry with the focuses of Organic Synthetic and Analytical 
-        Chemistry and am currently undergoing study for web development. My 
-        intention is to find a job that will permit me to continue in or 
-        advance my data science skills. I'm currently working HTML, 
-        Javascript and CSS and plan on learning more programming languages 
-        to continue to develop webpages with more tools.
+        Chemistry and completed a UT Web Development Bootcamp as of January 2021.
+        Since then, I have been working on independent projects, building my 
+        experience with more languages and techniques. I have 1 year experience 
+        using advanced HTML, CSS and Javascript and have prepared full stack pages
+        using React, EJS, Node.js, MySQL and MongoDB. Now, my intention is to find
+        a job in web development that will permit me to continue this career.
         </p>
     </div>
     );
--- src/components/Pages/LatestWorks/index.js
+++ src/components/Pages/LatestWorks/index.js
@@ -21,12 +21,12 @@ const LatestWorks = () => {
           <div style = {style}>
               <h1 style ={{fontSize: "235%"}}>Latest Works</h1>     
               <div>
-          <a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/TeamProfileGenerator">Team Profile Generator</a><br/>
-          <a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/EmployeeTracker">Employee Tracker</a><br/>
-          <a style= {{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/employeedirectory">Employee Directory</a><br/>
-          <a style= {{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/CodeQuiz">Code Quiz</a><br/>
-          <a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/burger">burger</a><br/>
-          <a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/workoutTracker">Workout Tracker</a><br/>
+          <a style={{color: "rgb(63, 23, 63)"}} href="https://nameless-journey-03440.herokuapp.com/" target="_blank" rel="noreferrer">Journal</a><br/>
+          <a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/EmployeeTracker" target="_blank" rel="noreferrer">Employee Tracker</a><br/>
+          <a style= {{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/employeedirectory" target="_blank" rel="noreferrer">Employee Directory</a><br/>
+          <a style= {{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/CodeQuiz" target="_blank" rel="noreferrer">Code Quiz</a><br/>
+          <a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/burger" target="_blank" rel="noreferrer">burger</a><br/>
+          <a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/workoutTracker" target="_blank" rel="noreferrer">Workout Tracker</a><br/>
         </div>
           </div>
           );
