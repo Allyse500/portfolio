b6d7656bc2e5d09c49b0b9b688a325f3b117db19
Allyse <ayubired@gmail.com>
2021-12-08 19:03:29 -0600

    update portfolio autobiography

--- src/components/Pages/Home/About/AboutContent.js
+++ src/components/Pages/Home/About/AboutContent.js
@@ -23,8 +23,8 @@ const AboutContent = () => {
         <p >My name is Allyse D. Johnson. I graduated from The University of 
         Texas at San Antonio in Dec. 2016 with a Bachelor's of Science in
         Chemistry and completed a UT Web Development Bootcamp as of January 2021.
-        Since then, I have been working on independent projects, building my 
-        experience with more languages and techniques. I have 1 year experience 
+        Since then, I have been working on independent private projects, building
+        my experience with more languages and techniques. I have 1 year experience 
         using advanced HTML, CSS and Javascript and have prepared full stack pages
         using React, EJS, Node.js, MySQL and MongoDB. Now, my intention is to find
         a job in web development that will permit me to continue this career.
