b6fe3eef57dca7422c32c49fec271d8d1d88cabf
Allyse <ayubired@gmail.com>
2021-12-08 19:11:31 -0600

    correcting spelling error

--- src/components/Pages/Home/About/AboutContent.js
+++ src/components/Pages/Home/About/AboutContent.js
@@ -25,7 +25,7 @@ const AboutContent = () => {
         Chemistry and completed a UT Web Development Bootcamp as of January 2021.
         Since then, I have been working on independent private projects, building
         my experience with more languages and techniques. I have 1 year experience 
-        using advanced HTML, CSS and Javascript and have prepared full stack pages
+        using advanced HTML, CSS and JavaScript and have prepared full stack pages
         using React, EJS, Node.js, MySQL and MongoDB. Now, my intention is to find
         a job in web development that will permit me to continue this career.
         </p>
