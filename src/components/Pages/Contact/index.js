10f637ada6ff096c1b9ff9a7fbbf5095c53094b4
Allyse <ayubired@gmail.com>
2022-09-13 13:09:43 -0500

    adjust CSS standards for active page resizing

--- src/components/Pages/Contact/index.js
+++ src/components/Pages/Contact/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import "../../../index.css"
+import "./contact.css"
 
 const Contact = () => {
     return (
         <div id="paragraph">
-            <h2 style ={{fontSize:"220%"}}>Contact Information</h2>
-            <p>Phone: 737-224-1626</p>
-            <p>email: ayubired@gmail.com</p>
-            <p>Find me on <a style={{color: "green"}} href= "https://www.linkedin.com/in/allyse-johnson-3b7a84116/">LinkedIn</a></p>
+            <h1>Contact Information</h1>
+            <div>Phone: 737-224-1626</div>
+            <div>email: ayubired@gmail.com</div>
+            <div>Find me on <a style={{color: "green"}} href= "https://www.linkedin.com/in/allyse-johnson-3b7a84116/">LinkedIn</a></div>
         </div>
         )
         }
--- src/components/Pages/Home/About/AboutContent.js
+++ src/components/Pages/Home/About/AboutContent.js
@@ -6,11 +6,11 @@ const AboutContent = () => {
     
     return (   
     <div id="paragraph">
-        <h1 style ={{fontSize: "220%"}}>Autobiography</h1>     
+        <h1 id="title">Autobiography</h1>     
         <p >My name is Allyse D. Johnson. I graduated from UTSA in Dec. 2016 with
         a B.S. in Chemistry and completed a UT Web Development Bootcamp as of 
         January 2021. Since then, I have been working on independent, private projects,
-        building my experience with more languages and techniques. I have about 1.5 years 
+        building my experience with more languages and techniques. I have about 2 years 
         experience using advanced HTML, CSS and JavaScript and have prepared full stack pages
         using React, EJS, Node.js, PHP, MySQLi and MongoDB. Now, my intention is to find
         a job in web development that will permit me to continue this career.
