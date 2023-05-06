3ee8b5911f3e2ccd874e7244f487b699b1da1c66
Allyse <ayubired@gmail.com>
2023-05-06 06:23:17 -0500

    updated portfolio links

--- src/components/Pages/LatestWorks/index.js
+++ src/components/Pages/LatestWorks/index.js
@@ -12,7 +12,7 @@ const LatestWorks = () => {
               <a style={{color: "rgb(63, 23, 63)"}} href="https://floating-basin-71739.herokuapp.com/" target="_blank" rel="noreferrer">Cookie Dough2: EJS, MongoDB</a><span>&nbsp;|&nbsp;</span><a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/cookie_dough2" target="_blank" rel="noreferrer">GitHub</a><br/>
           <a style={{color: "rgb(63, 23, 63)"}} href="https://hidden-savannah-06355.herokuapp.com/" target="_blank" rel="noreferrer">Google Books Search: React, MongoDB</a><span>&nbsp;|&nbsp;</span><a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/googlebookssearch" target="_blank" rel="noreferrer">GitHub</a><br/>
           <a style= {{color: "rgb(63, 23, 63)"}} href="https://allyse500.github.io/CodeQuiz/" target="_blank" rel="noreferrer">Code Quiz: HTML, CSS, JavaScript</a><span>&nbsp;|&nbsp;</span><a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/CodeQuiz" target="_blank" rel="noreferrer">GitHub</a><br/>
-          <a style={{color: "rgb(63, 23, 63)"}} href="https://safe--route.herokuapp.com/" target="_blank" rel="noreferrer">Safe Route: React, MySQL</a><span>&nbsp;|&nbsp;</span><a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/destaneelg/SafeRoute" target="_blank" rel="noreferrer">GitHub</a><br/>
+          <a style={{color: "rgb(63, 23, 63)"}} href="https://nameless-journey-03440.herokuapp.com/" target="_blank" rel="noreferrer">Journal: EJS, MongoDB</a><span>&nbsp;|&nbsp;</span><a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/notepadMongo" target="_blank" rel="noreferrer">GitHub</a><br/>
         </div>
           </div>
           );
