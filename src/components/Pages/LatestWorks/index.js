8f0f850374b509416f45b5c8cda76f5580d8fe79
Allyse <ayubired@gmail.com>
2023-05-19 09:52:08 -0500

    rename "Eight Ball" link on latest works to "Red Eight Ball"

--- src/components/Pages/LatestWorks/index.js
+++ src/components/Pages/LatestWorks/index.js
@@ -13,7 +13,7 @@ const LatestWorks = () => {
           <a style={{color: "rgb(63, 23, 63)"}} href="https://hidden-savannah-06355.herokuapp.com/" target="_blank" rel="noreferrer">Google Books Search: React, MongoDB</a><span>&nbsp;|&nbsp;</span><a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/googlebookssearch" target="_blank" rel="noreferrer">GitHub</a><br/>
           <a style= {{color: "rgb(63, 23, 63)"}} href="https://allyse500.github.io/CodeQuiz/" target="_blank" rel="noreferrer">Code Quiz: HTML, CSS, JavaScript</a><span>&nbsp;|&nbsp;</span><a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/CodeQuiz" target="_blank" rel="noreferrer">GitHub</a><br/>
           <a style={{color: "rgb(63, 23, 63)"}} href="https://nameless-journey-03440.herokuapp.com/" target="_blank" rel="noreferrer">Journal: EJS, MongoDB</a><span>&nbsp;|&nbsp;</span><a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/notepadMongo" target="_blank" rel="noreferrer">GitHub</a><br/>
-          <a style={{color: "rgb(63, 23, 63)"}} href="https://gentle-citadel-53498.herokuapp.com/ " target="_blank" rel="noreferrer">Eight Ball: React</a><span>&nbsp;|&nbsp;</span><a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/eight_ball " target="_blank" rel="noreferrer">GitHub</a><br/>
+          <a style={{color: "rgb(63, 23, 63)"}} href="https://gentle-citadel-53498.herokuapp.com/ " target="_blank" rel="noreferrer">Red Eight Ball: React</a><span>&nbsp;|&nbsp;</span><a style={{color: "rgb(63, 23, 63)"}} href="https://github.com/Allyse500/eight_ball " target="_blank" rel="noreferrer">GitHub</a><br/>
         </div>
           </div>
           );
